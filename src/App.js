95d85709e97e57692f80d07f9292f6672192b317
Diya Sundeep <diyasundeep64@gmail.com>
2023-11-15 20:50:07 +0530

    todo_components added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-
-import SideBar from './components/SideBar';
-import TopBar from './components/TopBar';
+import SideBar from "./components/SideBar";
+import TopBar from "./components/TopBar";
 function App() {
- 
-
   return (
-    <div className='bg-gray-100 h-screen'>
-      <TopBar/>
+    <div className="">
+      <TopBar />
       <div>
-    <SideBar/>
-        <div className='md:w-5/6 h-screen md:absolute md:right-0 bg-gray-100'>
-         
-        <div className='text-2xl self-center text-center items-center justify-center  font-semibold '>
-          hello world!
-          
+        <SideBar />
+        <div className="md:w-5/6 h-screen md:absolute md:right-0 bg-gray-100">
+          <div className="text-2xl self-center text-center items-center justify-center font-semibold  bg-gray-100">
+            <div className="flex flex-wrap -mx-4 -my-4 ">
+              {[...Array(10)].map((_, index) => (
+                <div key={index} className="w-1/3 px-4 py-4">
+                  <div className="bg-gray-300 h-40 flex flex-col items-center justify-center rounded-md">
+                    <p className="text-xl font-bold mb-2">Todo {index + 1}</p>
+                    <p className="text-sm">Do the todo thingy asap.</p>
+                  </div>
+                </div>
+              ))}
+              <div className="w-1/3 px-4 py-4">
+                <a className="bg-gray-300 h-40 flex flex-col items-center justify-center rounded-md hover:bg-gray-200">
+                  <img
+                    src="https://static.thenounproject.com/png/2310577-200.png"
+                    className="h-8 w-8"
+                  ></img>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-   
       </div>
-    </div >
-
+    </div>
   );
 }
 
 export default App;
-
-
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,22 +1,30 @@
 function TopBar() {
-    return (
-        <nav class="bg-white border-gray-200 shadow-sm">
-            <div class='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:p-7 p-10 '>
-                <a class="hidden md:block flex  items-center">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap uppercase ">logo</span>
-                </a>
-                <div className="hidden lg:block font-semibold whitespace-nowrap text-2xl">
-                    Hi , guys !
-                </div>
-                <div class="flex absolute right-10 top-6 md:flex md:justify-center ">
-                    <button type="button" id="menu button" class=" focus:ring-4 focus:ring-gray-300 " >
-                        <img class="w-10 h-10 rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGf_8UZ3xLijdkOtv3qWnUpyknARbKMrcVJA&usqp=CAU" >
-                        </img>
-                    </button>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav class="bg-white border-gray-200 shadow-sm">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto md:p-7 p-10 ">
+        <a class="hidden md:block flex  items-center">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap uppercase ">
+            logo
+          </span>
+        </a>
+        <div className="hidden lg:block font-semibold whitespace-nowrap text-2xl">
+          Sticky Notes
+        </div>
+        <div class="flex absolute right-10 top-6 md:flex md:justify-center ">
+          <button
+            type="button"
+            id="menu button"
+            class=" focus:ring-4 focus:ring-gray-300 "
+          >
+            <img
+              class="w-10 h-10 rounded-full"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGf_8UZ3xLijdkOtv3qWnUpyknARbKMrcVJA&usqp=CAU"
+            ></img>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default TopBar;
